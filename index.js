3567357482ace57209bf02fb372dc1711f37cd9c
Cezary Wojtkowski <cezary@gmail.com>
2017-08-02 15:47:37 -0700

    clean up

--- index.js
+++ index.js
@@ -7,16 +7,21 @@ const cors = microCors({
   allowMethods: ['GET']
 });
 
-const handler = async (req, res) => {
+const FAVICON_REGEX = /\/(favicon|(apple-)?touch-icon(-i(phone|pad))?(-\d{2,}x\d{2,})?(-precomposed)?)\.(jpe?g|png|ico|gif)$/i;
+
+const noFavicon = handler => (req, res) => {
   // return nothing for requests for favicon
-  if (req.url === '/favicon.ico') {
-    res.statusCode = 204;
+  if (FAVICON_REGEX.test(req.url)) {
+    res.statusCode = 404;
     return null;
   }
 
+  return handler(req, res);
+}
+
+const handler = async (req, res) => {
   const { query } = parseUrl(req.url, true);
   const ip = query.ip || getClientIp(req);
-
   const info = geoip.lookup(ip);
   const { ll, country, region, city, metro, zip } = info;
   const [latitude, longitude] = ll;
@@ -30,4 +35,4 @@ const handler = async (req, res) => {
 };
 
 
-module.exports = cors(handler);
+module.exports = noFavicon(cors(handler));
